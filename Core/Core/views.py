bc6af070247d8b0c29834e64a8c4fddd0f5da3c4
Juan-H18 <hernandez.juanjose2005@gmail.com>
2024-06-15 14:47:49 -0500

    15/06/2024

--- Core/Core/views.py
+++ Core/Core/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import CustomAuthenticationForm 
+from Usuarios.models import Usuarios
+from django.contrib import messages
+
 
 def index(request):
     return render(request, 'index.html')
@@ -6,5 +11,21 @@ def index(request):
 def about(request):
     return render(request, 'SobreNosotros.html')
 
-def login(request):
-    return render(request, 'Login.html')
+
+
+def custom_login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request.POST)
+        if form.is_valid():
+            Documento = form.cleaned_data.get('Documento')
+            Rol = form.cleaned_data.get('Rol')
+            user = Usuarios.objects.filter(Documento=Documento, Rol=Rol).first()
+            if user is not None and user.Rol == 2:
+                login(request, user)
+                messages.success(request, f'¡Bienvenido, {user.Nombre}! Has iniciado sesión correctamente.')
+                return redirect('/admin/')  # Cambia 'Productos' a la URL correcta
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'registration/Login.html', {'form': form})
+
+
