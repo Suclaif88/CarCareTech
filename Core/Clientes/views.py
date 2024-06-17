e18f3c40f4cbb891a458c665705f65339efad570
Mona Lisa <roldansebasdaza@gmail.com>
2024-06-16 20:57:42 -0500

    16/06/2024

--- Core/Clientes/views.py
+++ Core/Clientes/views.py
@@ -1,20 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-<<<<<<< HEAD
-from .models import Clientes
-from .forms import ClienteForm
-from django.http import JsonResponse
-
-
-#----------VISTA ADMIN CLIENTES------------
-
-def AdClientes(request):
-    cliente = Clientes.objects.all()
-    return render(request, 'ADMIN/clientes.html', {'clientes':cliente})
-
-
-
-#-------------EDITAR CLIENTE---------
-=======
 from django.contrib import messages
 from .models import Clientes
 from django.contrib.auth import logout
@@ -31,60 +15,11 @@ def AdClientes(request):
     return render(request, 'ADMIN/clientes.html', {'clientes': clientes})
 
 #-----EDITAR-----------------------
->>>>>>> b5919f5e10669152a5ac66012a1cec9fc9246f53
 
 def editar_cliente(request, documento):
     cliente = get_object_or_404(Clientes, Documento=documento)
 
     if request.method == 'POST':
-<<<<<<< HEAD
-        nombre = request.POST.get('nombre')
-        apellido = request.POST.get('apellido')
-        celular = request.POST.get('celular')
-        f_nacimiento = request.POST.get('f_nacimiento')
-        correo = request.POST.get('correo')
-        direccion = request.POST.get('direccion')
-
-        cliente.Nombre = nombre
-        cliente.Apellido = apellido
-        cliente.Celular = celular
-        cliente.F_Nacimiento = f_nacimiento
-        cliente.Correo = correo
-        cliente.Direccion = direccion
-
-        cliente.save()
-
-        return redirect('ad_clientes')
-
-    return render(request, 'ADMIN/editar_cliente.html', {'cliente': cliente})
-
-
-#--------ELIMINAR----------
-
-def eliminar_cliente(request, documento):
-    cliente = get_object_or_404(Clientes, Documento=documento)
-
-    if request.method == 'POST':
-        cliente.delete()
-        return redirect('ad_clientes')
-
-    return render(request, 'ADMIN/eliminar_cliente.html', {'cliente': cliente})
-
-#-----AGREGAR CLIENTE------------
-
-def agregar_cliente(request):
-    if request.method == 'POST':
-        form = ClienteForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return JsonResponse({'message': 'Cliente agregado correctamente'})
-        else:
-            return JsonResponse({'errors': form.errors}, status=400)
-    else:
-        form = ClienteForm()
-    
-    return render(request, 'ADMIN/clientes.html', {'form': form})
-=======
         try:
             data = json.loads(request.body.decode('utf-8'))
 
@@ -159,4 +94,3 @@ def agregar_cliente(request):
                 return JsonResponse({'error': str(e)}, status=500)
 
     return JsonResponse({'error': 'Método no permitido'}, status=405)
->>>>>>> b5919f5e10669152a5ac66012a1cec9fc9246f53
